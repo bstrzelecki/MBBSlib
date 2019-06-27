c280e4e7520f096a3ab7ff1b8f75b0a6a83d1839
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-06-27 13:58:40 +0200

    simple A* implementation

---
+++ MBBSlib/MBBSlib/AI/Pathfinding.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+
+namespace MBBSlib.AI
+{
+    class Pathfinding
+    {
+        float[,] map;
+        int maxX = 0;
+        int maxY = 0;
+        public Pathfinding(float[,] map)
+        {
+            this.map = map;
+            maxX = map.GetUpperBound(0);
+            maxY = map.GetUpperBound(1);
+        }
+        float heuristic_cost_estimate(Point start, Point end)
+        {
+
+            return (float)Math.Sqrt(
+            Math.Pow(start.X -end.X, 2) +
+            Math.Pow(start.Y -end.Y, 2)
+        );
+
+
+        }
+        private List<Point> GetNeighbors(Point point) {
+            List<Point> points = new List<Point>();
+            int x = point.X;
+            int y = point.Y;
+            AddPoint(new Point(x + 1, y), points);
+            AddPoint(new Point(x - 1, y), points);
+            AddPoint(new Point(x, y + 1), points);
+            AddPoint(new Point(x, y - 1), points);
+            //TODO add more connections
+
+            return points;
+        }
+        private void AddPoint(Point p, List<Point> pp)
+        {
+            if (CheckPoint(p))
+                pp.Add(p);
+        }
+        private bool CheckPoint(Point p)
+        {
+            if (p.X >= 0 && p.X < maxX) return true;
+            if (p.Y >= 0 && p.Y < maxY) return true;
+            return false;
+        }
+        public List<Point> GetPath(Point start, Point end)
+        {
+            List<Point> evaluated = new List<Point>();
+
+            List<Point> discovered = new List<Point>();
+            discovered.Add(start);
+
+            Dictionary<Point, Point> origins = new Dictionary<Point, Point>();
+            Dictionary<Point, float> scores = new Dictionary<Point, float>();
+
+            scores.Add(start, 0);
+
+            Dictionary<Point, float> finalScores = new Dictionary<Point, float>();
+            for(int x = 0; x < map.GetUpperBound(0); x++)
+            {
+                for(int y = 0; y < map.GetUpperBound(1); y++)
+                {
+                    finalScores.Add(new Point(x, y), float.PositiveInfinity);
+                }
+            }
+
+            finalScores[start] = heuristic_cost_estimate(start, end);
+
+            while(discovered.Count > 0)
+            {
+                Point current = (from n in discovered orderby finalScores[n] descending select n).First();
+
+                if (current == end)
+                    return ReconstructPath(origins, current);
+
+                discovered.Remove(current);
+                evaluated.Add(current);
+
+                foreach(Point point in GetNeighbors(current))
+                {
+                    if (evaluated.Contains(point)) continue;
+
+                    float tScore = scores[current] + (Distance(current, point) * map[current.X, current.Y]);
+
+                    if (!discovered.Contains(point))
+                    {
+                        discovered.Add(point);
+                    }else if(tScore >= scores[point])
+                    {
+                        continue;
+                    }
+                    origins.Add(point, current);
+                    scores.Add(point, tScore);
+                    finalScores[point] = scores[point] + heuristic_cost_estimate(point, end);
+                }
+            }
+            throw new Exception("Something went wrong");
+        }
+        private float Distance(Point a, Point b)
+        {
+            var ac = (double)(b.X - a.X);
+            var bc = (double)(b.Y - a.Y);
+
+            return (float)Math.Sqrt(ac * ac + bc * bc);
+        }
+
+        private List<Point> ReconstructPath(Dictionary<Point, Point> origins, Point current)
+        {
+            List<Point> path = new List<Point>();
+            path.Add(current);
+            while (origins.ContainsKey(current))
+            {
+                current = origins[current];
+                path.Add(current);
+            }
+            return path;
+        }
+    }
+}
---
+++ MBBSlib/MBBSlib/AI/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.AI
+{
+    public struct Point
+    {
+        public int X, Y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static bool operator ==(Point a, Point b)
+        {
+            if (a.X == b.X && a.Y == b.Y)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            if (a.X != b.X || a.Y != b.Y)
+                return true;
+            return false;
+        }
+    }
+}
