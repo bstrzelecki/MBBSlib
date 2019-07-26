2fe89898f52b7fe7a3f10fb035d0fb77dfefc044
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-07-26 16:17:25 +0200

    moved to lib

---
+++ MBBSlib/MBBSlibMonoGame/MultiSprite.cs
@@ -0,0 +1,25 @@
+﻿namespace MBBSlib.MonoGame
+{
+    public class MultiSprite : Sprite
+    {
+        public string Variant { get; protected set; } = string.Empty;
+
+        public void SetTextureVariant(string variant)
+        {
+            Variant = variant;
+
+            if (Variant == string.Empty || !TextureStorage.ContainsTextureKey($"{textureName}_{Variant}"))
+            {
+                Texture = TextureStorage.GetTexture(textureName);
+            }
+            else
+            {
+                Texture = TextureStorage.GetTexture($"{textureName}_{Variant}");
+            }
+
+        }
+        public MultiSprite(string sprite) : base(sprite)
+        {
+        }
+    }
+}
