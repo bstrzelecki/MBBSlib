2807a60ece757f030cd9b76e83502fdc251b283a
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-09-25 19:06:32 +0200

    added window.cs

---
+++ MBBSlib/MBBSlibMonoGame/UI/Window.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.MonoGame.UI
+{
+    class Window : ContentPane
+    {
+        public Window()
+        {
+        }
+
+    }
+}
