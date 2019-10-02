1ef3c4a04866fae15f03f77c355daa27bea9f272
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-10-02 19:28:20 +0200

    achievement data type

---
+++ MBBSlib/MBBSlib/Integrations/Achievement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.Integrations
+{
+    class Achievement
+    {
+        public string Title { get; }
+        public string Descrition { get; }
+        public string Key { get; set; }
+        public event Action OnAchievementTriggered;
+        public Achievement(string title, string desc)
+        {
+            Title = title;
+            Descrition = desc;
+        }
+        public void Trigger()
+        {
+            OnAchievementTriggered?.Invoke();
+        }
+    }
+}
