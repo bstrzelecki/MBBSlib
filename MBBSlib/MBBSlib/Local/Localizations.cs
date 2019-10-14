55c6edf49622b831f8f92e0e564eb41c9c8f5ed1
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-10-14 16:02:11 +0200

    added local

---
+++ MBBSlib/MBBSlib/Local/Localizations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.Local
+{
+    public static class Localizations
+    {
+        static Dictionary<string, string> locals = new Dictionary<string, string>();
+
+        public static void LoadTranslation(string fileName)
+        {
+            //TODO
+        }
+        public static string T(this string t)
+        {
+            if (!locals.ContainsKey(t))
+            {
+                return t;
+            }
+            return locals[t];
+        }
+        public static string Translate(this string t)
+        {
+            if (!locals.ContainsKey(t))
+            {
+                return t;
+            }
+            return locals[t];
+        }
+    }
+}
