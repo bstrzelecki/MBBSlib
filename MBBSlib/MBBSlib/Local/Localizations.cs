e94c42522a3558ff00e0111be4a6354cf56aae2a
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-10-14 16:12:36 +0200

    commented code

--- MBBSlib/MBBSlib/Local/Localizations.cs
+++ MBBSlib/MBBSlib/Local/Localizations.cs
@@ -8,9 +8,13 @@ namespace MBBSlib.Local
     public static class Localizations
     {
         static Dictionary<string, string> locals = new Dictionary<string, string>();
-
+        /// <summary>
+        /// Loads and fills strings to RAM
+        /// </summary>
+        /// <param name="fileName">Direct path to lang.xml file</param>
         public static void LoadTranslation(string fileName)
         {
+            locals.Clear();
             XDocument doc = XDocument.Load(fileName);
             XElement root = doc.Root;
 
@@ -19,6 +23,11 @@ namespace MBBSlib.Local
                 locals.Add(n.Element("key").Value, n.Element("string").Value);
             }
         }
+        /// <summary>
+        /// alias for Translate()
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static string T(this string t)
         {
             if (!locals.ContainsKey(t))
@@ -27,6 +36,11 @@ namespace MBBSlib.Local
             }
             return locals[t];
         }
+        /// <summary>
+        /// Replaces key with coresponding string in dictionary
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static string Translate(this string t)
         {
             if (!locals.ContainsKey(t))
