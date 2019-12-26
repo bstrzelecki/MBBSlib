d4c945bf1225499df989f1083f2b1d6862e24843
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-12-26 16:17:35 +0100

    added font class

---
+++ MBBSlib/MBBSlibMonoGame/Font.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace MBBSlib.MonoGame
+{
+    public class Font
+    {
+        readonly string font = string.Empty;
+        public Font(string fontName)
+        {
+            font = fontName;
+        }
+
+        public static implicit operator SpriteFont(Font font)
+        {
+            return GameMain.fonts[font.font];
+        }
+    }
+}
