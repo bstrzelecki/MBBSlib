7d447042660f6916bc54ba10c17ce9ad652e9d97
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-02-02 17:35:48 +0100

    setting loader

---
+++ MBBSlib/MBBSlib/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MBBSlib
+{
+    class Settings
+    {
+        static string filePath = Environment.CurrentDirectory;
+
+        static XElement settings;
+
+        public static void Load()
+        {
+            XDocument doc = XDocument.Load(filePath + "settings.xml");
+            settings = doc.Root;
+        }
+
+        public static string GetSetting(string key)
+        {
+            return settings.Element(key).Value;
+        }
+    }
+}
