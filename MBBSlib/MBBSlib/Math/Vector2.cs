990c2af07b7bb8e7eff2a29816724da2d85a777f
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-02-03 19:45:06 +0100

    minor fix

---
+++ MBBSlib/MBBSlib/Math/Vector2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.Math
+{
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
