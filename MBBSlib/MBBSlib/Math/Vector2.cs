1214cb62d8e04884e3fa5ae2111cc9d7067c8f7e
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-02-04 19:42:06 +0100

    minor feature

--- MBBSlib/MBBSlib/Math/Vector2.cs
+++ MBBSlib/MBBSlib/Math/Vector2.cs
@@ -6,6 +6,15 @@ namespace MBBSlib.Math
 {
     public struct Vector2
     {
+        public static Vector2 Down { get { return new Vector2(0, -1); } }
+        public static Vector2 Up { get { return new Vector2(0, 1); } }
+        public static Vector2 One { get { return new Vector2(1, 1); } }
+        public static Vector2 Left { get { return new Vector2(-1, 0); } }
+        public static Vector2 Right { get { return new Vector2(1, 0); } }
+        public static Vector2 Zero { get { return new Vector2(0, 0); } }
+
+        public float Magnitude { get { return (float)(System.Math.Sqrt((x * x) + (y * y))); } }
+
         public float x;
         public float y;
 
