68880e2b7cc0612fa7dab15fc7b347705714198d
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-02-25 18:45:32 +0100

    removed unnessesary statements

--- MBBSlib/MBBSlib/Utility/Sorter.cs
+++ MBBSlib/MBBSlib/Utility/Sorter.cs
@@ -6,5 +6,17 @@ namespace MBBSlib.Utility
 {
     class Sorter
     {
+        List<int> a = new List<int>();
+        public Sorter()
+        {
+            
+        }
+    }
+    public static class SortExtensions
+    {
+        public static IEnumerable<T> S<T>(this IEnumerable<T> tab)
+        {
+            return tab;
+        }
     }
 }
