2fc0c4e710a538d3a19d8c31407ebf6817b4cbe7
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-02-28 19:22:38 +0100

    minor changes

---
+++ MBBSlib/MBBSlibMonoGame/Sounds.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MBBSlib.MonoGame
+{
+    class Sounds
+    {
+        public static void PlayOneShot(SoundEffect effect)
+        {
+
+        }
+        public static void Loop(Song song)
+        {
+
+        }
+    }
+}
