7846d2b2c9df7456cd22c7b984d7fd67ab6ff754
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-03-08 19:58:14 +0100

    code claenup

--- MBBSlib/MBBSlibMonoGame/GameRegistry.cs
+++ MBBSlib/MBBSlibMonoGame/GameRegistry.cs
@@ -28,22 +28,6 @@ namespace MBBSlib.MonoGame
                 layer = l;
                 drawable = draw;
             }
-            public override bool Equals(object obj)
-            {
-                if (obj is Renderer r)
-                {
-                    return ((r.layer == layer) && (r.drawable == drawable));
-                }
-                return false;
-            }
-
-            public override int GetHashCode()
-            {
-                var hashCode = -628141515;
-                hashCode = hashCode * -1521134295 + layer.GetHashCode();
-                hashCode = hashCode * -1521134295 + EqualityComparer<IDrawable>.Default.GetHashCode(drawable);
-                return hashCode;
-            }
         }
     }
 }
