39ed4f4861b370cebab5f4b3dc96300412c16ee3
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-04-03 12:59:40 +0200

    audio lazyload

--- MBBSlib/MBBSlibMonoGame/Sounds.cs
+++ MBBSlib/MBBSlibMonoGame/Sounds.cs
@@ -1,24 +1,40 @@
 ﻿using Microsoft.Xna.Framework.Media;
 using System;
+using System.Collections.Generic;
+
 namespace MBBSlib.MonoGame
 {
     public static class Sounds
     {
         public static float Volume { get { return MediaPlayer.Volume; } set { MediaPlayer.Volume = value; } }
+        private static Dictionary<string, Song> songs = new Dictionary<string, Song>();
         public static void Loop(Song song)
         {
             MediaPlayer.Play(song);
             MediaPlayer.MediaStateChanged += MediaPlayer_MediaStateChanged;
         }
         public static void Loop(string song)
-        {
-            Song s = Song.FromUri(song, new Uri(Environment.CurrentDirectory + @"Content\Audio\" + song));
+        { 
+            Song s = GetSong(song);
             Loop(s);
         }
         public static void Play(string song)
         {
-            Song s = Song.FromUri(song, new Uri(Environment.CurrentDirectory + @"Content\Audio\" + song));
-            MediaPlayer.Play(s)
+            Song s = GetSong(song);
+            MediaPlayer.Play(s);
+        }
+        private static Song GetSong(string song)
+        {
+            if (songs.ContainsKey(song))
+            {
+                return songs[song];
+            }
+            else
+            {
+                Song s = Song.FromUri(song, new Uri(Environment.CurrentDirectory + @"Content\Audio\" + song));
+                songs.Add(song, s);
+                return s;
+            }
         }
         private static Song _currentSong;
         private static void MediaPlayer_MediaStateChanged(object sender, EventArgs e)
