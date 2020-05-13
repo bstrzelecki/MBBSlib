57d1dbaf51d2e9f8509a704e64cdd5ed24ed1cae
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-05-13 16:08:05 +0200

    code refactoring

--- MBBSlib/MBBSlibMonoGame/RenderBatch.cs
+++ MBBSlib/MBBSlibMonoGame/RenderBatch.cs
@@ -6,72 +6,72 @@ namespace MBBSlib.MonoGame
 {
     public class RenderBatch : IDisposable
     {
-        readonly SpriteBatch spriteBatch;
-        readonly GraphicsDevice graphicsDevice;
+        readonly SpriteBatch _spriteBatch;
+        readonly GraphicsDevice _graphicsDevice;
         public RenderBatch(SpriteBatch sb, GraphicsDevice gd)
         {
-            spriteBatch = sb;
-            graphicsDevice = gd;
+            _spriteBatch = sb;
+            _graphicsDevice = gd;
         }
         public void DrawRelative(Texture2D texture, Math.Vector2 position)
         {
-            Vector2 p = new Vector2(position.x, position.y);
-            spriteBatch.Draw(texture, p - GameMain.Instance.camera2D.Position, Color.White);
+            var p = new Vector2(position.x, position.y);
+            _spriteBatch.Draw(texture, p - GameMain.Instance.camera2D.Position, Color.White);
         }
         public void DrawRelative(Texture2D texture, Math.Vector2 position, Color color)
         {
-            Vector2 p = new Vector2(position.x, position.y);
-            spriteBatch.Draw(texture, p - GameMain.Instance.camera2D.Position, color);
+            var p = new Vector2(position.x, position.y);
+            _spriteBatch.Draw(texture, p - GameMain.Instance.camera2D.Position, color);
         }
         public void Draw(Texture2D texture, Rectangle size)
         {
-            spriteBatch.Draw(texture, size, Color.White);
+            _spriteBatch.Draw(texture, size, Color.White);
         }
         public void Draw(Texture2D texture, Rectangle size, Color color)
         {
-            spriteBatch.Draw(texture, size, color);
+            _spriteBatch.Draw(texture, size, color);
         }
         public void Draw(Texture2D texture, Vector2 position)
         {
-            spriteBatch.Draw(texture, position, Color.White);
+            _spriteBatch.Draw(texture, position, Color.White);
         }
         public void Draw(Texture2D texture, Math.Vector2 position, Color color)
         {
-            spriteBatch.Draw(texture, new Vector2(position.x,position.y), color);
+            _spriteBatch.Draw(texture, new Vector2(position.x,position.y), color);
         }
         public void Draw(Texture2D texture, Math.Vector2 position)
         {
-            Vector2 p = new Vector2(position.x, position.y);
-            spriteBatch.Draw(texture, p, Color.White);
+            var p = new Vector2(position.x, position.y);
+            _spriteBatch.Draw(texture, p, Color.White);
         }
         public void Draw(string texture, Math.Vector2 position)
         {
-            Vector2 p = new Vector2(position.x, position.y);
-            spriteBatch.Draw(new Sprite(texture), p, Color.White);
+            var p = new Vector2(position.x, position.y);
+            _spriteBatch.Draw(new Sprite(texture), p, Color.White);
         }
         public void Draw(Texture2D texture, Math.Vector2 position, float rotation, Math.Vector2 origin)
         {
-            spriteBatch.Draw(new Sprite(texture), new Rectangle((int)position.x, (int)position.y, texture.Width, texture.Height), null, Color.White, rotation, new Vector2(origin.x, origin.y), SpriteEffects.None, 0);
+            _spriteBatch.Draw(new Sprite(texture), new Rectangle((int)position.x, (int)position.y, texture.Width, texture.Height), null, Color.White, rotation, new Vector2(origin.x, origin.y), SpriteEffects.None, 0);
         }
         public void Draw(Texture2D texture, Vector2 position, Color color)
         {
-            spriteBatch.Draw(texture, position, color);
+            _spriteBatch.Draw(texture, position, color);
         }
         public void Draw(string textureName, Vector2 position)
         {
-            spriteBatch.Draw(new Sprite(textureName), position, Color.White);
+            _spriteBatch.Draw(new Sprite(textureName), position, Color.White);
         }
         public void Draw(string textureName, Vector2 position, Color color)
         {
-            spriteBatch.Draw(new Sprite(textureName), position, color);
+            _spriteBatch.Draw(new Sprite(textureName), position, color);
         }
         public void DrawString(SpriteFont font, string text, Vector2 position, Color color)
         {
-            spriteBatch.DrawString(font, text, position, color);
+            _spriteBatch.DrawString(font, text, position, color);
         }
         public void DrawString(string text, Vector2 position, Color color)
         {
-            spriteBatch.DrawString(new Font("font"), text, position, color);
+            _spriteBatch.DrawString(new Font("font"), text, position, color);
         }
         public void DrawMesh(Model model, Vector3 position)
         {
@@ -94,11 +94,12 @@ namespace MBBSlib.MonoGame
             {
                 foreach (BasicEffect effect in mesh.Effects)
                 {
-                    effect.EnableDefaultLighting();
-                    effect.AmbientLightColor = new Vector3(0, 0, 0);
                     effect.View = GameMain.Instance.camera3D.viewMatrix;
-                    effect.World = Matrix.CreateTranslation(position);
                     effect.Projection = GameMain.Instance.camera3D.projectionMatrix;
+
+                    effect.AmbientLightColor = new Vector3(0, 0, 0);
+                    effect.EnableDefaultLighting();
+                    effect.World = Matrix.CreateTranslation(position);
                     effect.TextureEnabled = true;
                     effect.Texture = texture;
                 }
@@ -107,90 +108,52 @@ namespace MBBSlib.MonoGame
         }
         public void DrawPrimitives(VertexPositionTexture[] mesh, Texture2D texture, bool disableCulling = false)
         {
-            var effect = new BasicEffect(graphicsDevice)
-            {
-                View = GameMain.Instance.camera3D.viewMatrix,
-                Projection = GameMain.Instance.camera3D.projectionMatrix,
-                TextureEnabled = true,
-                Texture = texture
-            };
+            var effect = GetBasicEffect(_graphicsDevice);
+            effect.TextureEnabled = true;
+            effect.Texture = texture;
+            
             if (!disableCulling)
             {
-                RasterizerState rasterizerState = new RasterizerState
-                {
-                    CullMode = CullMode.None
-                };
-                graphicsDevice.RasterizerState = rasterizerState;
+                _graphicsDevice.RasterizerState = GetPrimitiveRasterizerState(disableCulling);
             }
-            var vertexBuffer = new VertexBuffer(graphicsDevice, typeof(VertexPositionTexture), mesh.Length, BufferUsage.WriteOnly);
+            var vertexBuffer = new VertexBuffer(_graphicsDevice, typeof(VertexPositionTexture), mesh.Length, BufferUsage.WriteOnly);
             vertexBuffer.SetData(mesh);
-            graphicsDevice.SetVertexBuffer(vertexBuffer);
+            _graphicsDevice.SetVertexBuffer(vertexBuffer);
             foreach (var pass in effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, mesh.Length);
+                _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, mesh.Length);
             }
         }
         public void DrawPrimitives(VertexPositionTexture[] mesh, Vector3 position, Texture2D texture, bool disableCulling = false)
         {
-            var effect = new BasicEffect(graphicsDevice)
-            {
-                View = GameMain.Instance.camera3D.viewMatrix,
-                Projection = GameMain.Instance.camera3D.projectionMatrix,
-                TextureEnabled = true,
-                Texture = texture
-            };
-            if (!disableCulling)
-            {
-                var rasterizerState = new RasterizerState
-                {
-                    CullMode = CullMode.None
-                };
-                graphicsDevice.RasterizerState = rasterizerState;
-            }
-            for (int i = 0; i < mesh.Length; i++)
+            for(int i = 0; i < mesh.Length; i++)
             {
                 mesh[i].Position += position;
             }
-            var vertexBuffer = new VertexBuffer(graphicsDevice, typeof(VertexPositionTexture), mesh.Length, BufferUsage.WriteOnly);
-            vertexBuffer.SetData(mesh);
-            graphicsDevice.SetVertexBuffer(vertexBuffer);
-            foreach (var pass in effect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, mesh.Length);
-            }
+            DrawPrimitives(mesh, texture, disableCulling);
         }
         public void DrawPrimitives(VertexPositionColor[] mesh, bool disableCulling = false)
         {
-            var effect = new BasicEffect(graphicsDevice)
-            {
-                View = GameMain.Instance.camera3D.viewMatrix,
-                Projection = GameMain.Instance.camera3D.projectionMatrix,
-                VertexColorEnabled = true,
-            };
+            var effect = GetBasicEffect(_graphicsDevice);
+            effect.VertexColorEnabled = true;
+            
             var dss = new DepthStencilState();
             dss.DepthBufferEnable = true;
+            _graphicsDevice.DepthStencilState = dss;
             
-            graphicsDevice.DepthStencilState = dss;
             if (!disableCulling)
             {
-                var rasterizerState = new RasterizerState
-                {
-                    CullMode = CullMode.None,
-                    FillMode = FillMode.Solid,
-                    DepthClipEnable = true
-                    
-                };
-                graphicsDevice.RasterizerState = rasterizerState;
+                _graphicsDevice.RasterizerState = GetPrimitiveRasterizerState(disableCulling);
             }
-            var vertexBuffer = new VertexBuffer(graphicsDevice, typeof(VertexPositionColor), mesh.Length, BufferUsage.WriteOnly);
+            var vertexBuffer = new VertexBuffer(_graphicsDevice, typeof(VertexPositionColor), mesh.Length, BufferUsage.WriteOnly);
             vertexBuffer.SetData(mesh);
-            graphicsDevice.SetVertexBuffer(vertexBuffer);
+            _graphicsDevice.SetVertexBuffer(vertexBuffer);
+
             foreach (var pass in effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, mesh.Length);
+                _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, mesh.Length);
             }
         }
         public void DrawPrimitives(VertexPositionColor[] mesh, Vector3 position, bool disableCulling = false)
@@ -201,19 +164,30 @@ namespace MBBSlib.MonoGame
             }
             DrawPrimitives(mesh, disableCulling);
         }
-
+        private BasicEffect GetBasicEffect(GraphicsDevice gd) => new BasicEffect(gd)
+        {
+            View = GameMain.Instance.camera3D.viewMatrix,
+            Projection = GameMain.Instance.camera3D.projectionMatrix
+        };
+        private RasterizerState GetPrimitiveRasterizerState(bool disableCulling) => new RasterizerState
+        {
+            CullMode = disableCulling?CullMode.None:CullMode.CullClockwiseFace,
+            FillMode = FillMode.Solid,
+            DepthClipEnable = true
+        };
         public void Dispose()
         {
-            spriteBatch.Dispose();
-            graphicsDevice.Dispose();
+            _spriteBatch.Dispose();
+            _graphicsDevice.Dispose();
         }
+
         public static explicit operator SpriteBatch(RenderBatch rb)
         {
-            return rb.spriteBatch;
+            return rb._spriteBatch;
         }
         public static explicit operator GraphicsDevice(RenderBatch rb)
         {
-            return rb.graphicsDevice;
+            return rb._graphicsDevice;
         }
     }
 }
