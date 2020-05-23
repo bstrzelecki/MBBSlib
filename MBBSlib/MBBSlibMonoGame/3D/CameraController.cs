e503389d7a7fc8304b29396f9557117413f4c6de
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-05-23 22:02:46 +0200

    minor fix

---
+++ MBBSlib/MBBSlibMonoGame/3D/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBBSlib.MonoGame._3D
+{
+    class CameraController : ICameraController
+    {
+        public ICameraController Default;
+    }
+    interface ICameraController
+    {
+
+    }
+}
