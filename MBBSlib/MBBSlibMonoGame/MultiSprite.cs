0b7a5243e7bb755bdac4dc0e904920e27cae277b
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-06-02 20:20:33 +0200

    multisprite comments

--- MBBSlib/MBBSlibMonoGame/MultiSprite.cs
+++ MBBSlib/MBBSlibMonoGame/MultiSprite.cs
@@ -1,9 +1,19 @@
 ﻿namespace MBBSlib.MonoGame
 {
+    /// <summary>
+    /// <see cref="Sprite"/> implementation for animated textures
+    /// </summary>
     public class MultiSprite : Sprite
     {
+        /// <summary>
+        /// Currently delected variant <see cref="string.Empty"/> for default one
+        /// </summary>
         public string Variant { get; protected set; } = string.Empty;
-
+        
+        /// <summary>
+        /// Allias for <see cref="Variant"/>
+        /// </summary>
+        /// <param name="variant">Variant to use, select <see cref="string.Empty"/> for default one</param>
         public void SetTextureVariant(string variant)
         {
             Variant = variant;
@@ -18,6 +28,10 @@
             }
 
         }
+        /// <summary>
+        /// Created pointer fo rtexture in registry
+        /// </summary>
+        /// <param name="sprite"></param>
         public MultiSprite(string sprite) : base(sprite)
         {
         }
