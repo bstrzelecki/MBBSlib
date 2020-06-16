263c0a33e175ebb178467d191a00f43c5ca37d98
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-06-16 20:14:29 +0200

    minor fix

--- MBBSlib/MBBSlibMonoGame/Time.cs
+++ MBBSlib/MBBSlibMonoGame/Time.cs
@@ -2,21 +2,36 @@
 
 namespace MBBSlib.MonoGame
 {
+    /// <summary>
+    /// Keeps track of physics updates
+    /// </summary>
     public static class Time
     {
-        public static int DeltaTime => tc.DeltaTime;
-        public static bool IsPaused { get => tc.IsPaused; set => tc.IsPaused = value; }
-        public static int TickTime { get => tc.TickTime; set => tc.TickTime = value; }
+        /// <summary>
+        /// Difference between frames in ms.
+        /// </summary>
+        public static int DeltaTime => _tc.DeltaTime;
+        /// <summary>
+        /// Is false when game time is passing
+        /// </summary>
+        public static bool IsPaused { get => _tc.IsPaused; set => _tc.IsPaused = value; }
+        /// <summary>
+        /// Number of ms between <see cref="OnTick"/> calls
+        /// </summary>
+        public static int TickTime { get => _tc.TickTime; set => _tc.TickTime = value; }
+        /// <summary>
+        /// Gets called every <see cref="TickTime"/> ms when <see cref="IsPaused"/> is false 
+        /// </summary>
         public static event Action OnTick;
-        internal static TickCounter tc;
-        internal static void Initialize() => tc = new TickCounter();
+        internal static TickCounter _tc;
+        internal static void Initialize() => _tc = new TickCounter();
         internal class TickCounter : IUpdateable
         {
             public int DeltaTime { get; private set; }
             public bool IsPaused { get; set; } = false;
             public int TickTime { get; set; } = 30;
-            private static int start;
-            private static int tickCounter;
+            private static int _start;
+            private static int _tickCounter;
             public TickCounter() => GameMain.RegisterUpdate(this);
             public void Update()
             {
@@ -25,13 +40,13 @@ namespace MBBSlib.MonoGame
                     DeltaTime = 0;
                     return;
                 }
-                DeltaTime = DateTime.Now.Millisecond - start;
+                DeltaTime = DateTime.Now.Millisecond - _start;
                 DeltaTime = DeltaTime > 0 ? DeltaTime : 0;
-                start = DateTime.Now.Millisecond;
-                tickCounter += DeltaTime;
-                if(tickCounter > TickTime)
+                _start = DateTime.Now.Millisecond;
+                _tickCounter += DeltaTime;
+                if(_tickCounter > TickTime)
                 {
-                    tickCounter = 0;
+                    _tickCounter = 0;
                     OnTick?.Invoke();
                 }
             }
