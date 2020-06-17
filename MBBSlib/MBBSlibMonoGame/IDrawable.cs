c098d8d6d923b3aaa318736b68a6320dc0b8fc6f
Mabat25 <48790127+mabat25@users.noreply.github.com>
2020-06-17 21:40:54 +0200

    minor fix

--- MBBSlib/MBBSlibMonoGame/IDrawable.cs
+++ MBBSlib/MBBSlibMonoGame/IDrawable.cs
@@ -1,7 +1,14 @@
 ﻿namespace MBBSlib.MonoGame
 {
+    /// <summary>
+    /// Interface for renderer tcicks
+    /// </summary>
     public interface IDrawable
     {
+        /// <summary>
+        /// Calls every <see cref="GameMain.Draw(Microsoft.Xna.Framework.GameTime)"/>
+        /// </summary>
+        /// <param name="sprite"></param>
         void Draw(RenderBatch sprite);
     }
 }
